116bddda9385d15b44210721d468e51ffd2e1c14
jonandres45 <ansa1315@outlook.es>
2022-05-05 11:09:05 -0500

    backend

--- backend/tablas.php
+++ backend/tablas.php
@@ -3,7 +3,11 @@
 require 'db.php';
 
 $con = connectionDB("laravel_capri", "root", "");
-$sentencia = $con->prepare("SELECT * FROM `tabla_ajuste` ORDER BY fecha DESC, `year`");
+$peticion = $con->prepare("SELECT fecha FROM `tabla_ajuste` GROUP BY fecha ORDER BY fecha DESC LIMIT 1");
+$peticion->execute();
+$ultimaFecha = $peticion->fetch();
+$sentencia = $con->prepare("SELECT * FROM `tabla_ajuste` WHERE fecha=:ultimaFecha ORDER BY fecha DESC, `year`");
+$sentencia->bindParam(":ultimaFecha", $ultimaFecha['fecha']);
 $sentencia->execute();
 $datos = $sentencia->fetchAll();
 echo json_encode($datos);
--- backend/tarifas.php
+++ backend/tarifas.php
@@ -2,7 +2,11 @@
 require 'db.php';
 
 $con = connectionDB("laravel_capri", "root", "");
-$sentencia = $con->prepare("SELECT * FROM `tarifas` ORDER BY fecha DESC, limite_inferior");
+$peticion = $con->prepare("SELECT fecha FROM `tarifas` GROUP BY fecha ORDER BY fecha DESC LIMIT 1");
+$peticion->execute();
+$ultimaFecha = $peticion->fetch();
+$sentencia = $con->prepare("SELECT * FROM `tarifas` WHERE fecha = :ultimaFecha ORDER BY fecha DESC, limite_inferior");
+$sentencia->bindParam(':ultimaFecha', $ultimaFecha['fecha']);
 $sentencia->execute();
 $datos = $sentencia->fetchAll();
 echo json_encode($datos);
